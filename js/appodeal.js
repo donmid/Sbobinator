f95311165fb60444b4ecede1844f7bd99fb7e408
widonwaker <hey.lol@live.it>
2016-11-09 11:26:27 +0100

    Update appodeal.js

--- js/appodeal.js
+++ js/appodeal.js
@@ -3,6 +3,6 @@ function onDeviceReady() {
 var appKey = "ce651cc71181061772443c14163325ec397e6e928243ffc8";
             Appodeal.disableLocationPermissionCheck();
             Appodeal.confirm(Appodeal.SKIPPABLE_VIDEO);
-            Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER);
+            Appodeal.initialize(appKey, Appodeal.INTERSTITIAL | Appodeal.SKIPPABLE_VIDEO | Appodeal.BANNER | Appodeal.REWARDED_VIDEO | Appodeal.NON_SKIPPABLE_VIDEO);
 
 }
